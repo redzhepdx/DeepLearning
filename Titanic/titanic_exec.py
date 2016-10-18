f4337f46d5dac27c5b660589f3c1051314fa69fc
redzhepdx <redzhepdx@users.noreply.github.com>
2016-10-18 13:07:49 +0300

    Update titanic_exec.py

--- Titanic/titanic_exec.py
+++ Titanic/titanic_exec.py
@@ -6,8 +6,8 @@ training_data , test_data = readFile.load_data()
 training_data = list(training_data)
 test_data = list(test_data)
 
-net = titanic_network.Network([5,200,200,100,50,2], cost=titanic_network.QuadraticCost)
-net.large_weight_initializer()
+net = titanic_network.Network([5,400,2], cost=titanic_network.QuadraticCost)
+net.default_weight_initializer()
 max_epoch = 50
 max_mini_batch_size = 30
 max_eta = 20
@@ -16,4 +16,4 @@ accuracies_e = []
 accuracies_t = []
 accuraciesInfos = []
 loop_Count = max_epoch*max_eta*max_mini_batch_size*max_lmbda
-e_c , e_a , t_c , t_a = net.SGD(training_data, 50, 50, 3.0, evaluation_data=test_data, lmbda = 1.0,monitor_evaluation_cost=True, monitor_evaluation_accuracy=True,monitor_training_cost=True, monitor_training_accuracy=True)
+e_c , e_a , t_c , t_a = net.SGD(training_data, 1500, 5, 0.01, evaluation_data=test_data, lmbda = 0.003,monitor_evaluation_cost=True, monitor_evaluation_accuracy=True,monitor_training_cost=True, monitor_training_accuracy=True)
